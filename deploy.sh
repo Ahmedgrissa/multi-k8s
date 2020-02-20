17df8d3f3f41d686078f63308a09b44c12b2f8ec
Ahmedgrissa <grissahmed@gmail.com>
2020-02-20 18:21:50 +0100

    add deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t ahmedgrissa/multi-client:latest -t ahmedgrissa/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t ahmedgrissa/multi-server:latest -t ahmedgrissa/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t ahmedgrissa/multi-worker:latest -t ahmedgrissa/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+docker push ahmedgrissa/multi-client:latest
+docker push ahmedgrissa/multi-server:latest
+docker push ahmedgrissa/multi-worker:latest
+docker push ahmedgrissa/multi-client:$SHA
+docker push ahmedgrissa/multi-server:$SHA
+docker push ahmedgrissa/multi-worker:$SHA
+
+kubectl apply -f k8s/
+kubectl set image deployments/server-deployment server=ahmedgrissa/multi-server:$SHA
+kubectl set image deployments/client-deployment client=ahmedgrissa/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=ahmedgrissa/multi-worker:$SHA
